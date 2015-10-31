0317a4f9be01e70b6b4571d3b9443763819203c9
GauthamGoli <goligautham@gmail.com>
2015-10-31 21:40:46 +0530

    Write comments

--- app/src/androidTest/java/com/phonelost/phonelost/ApplicationTest.java
+++ app/src/androidTest/java/com/phonelost/phonelost/ApplicationTest.java
@@ -6,6 +6,9 @@ import android.test.ApplicationTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+/**
+* Write some more comments for clarity dude :P 
+*/
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
